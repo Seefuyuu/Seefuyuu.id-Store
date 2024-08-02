31c860f5d0cf84e2b9afeaab7929a58a31ee0be9
Seefuyuu <118198151+Seefuyuu@users.noreply.github.com>
2024-08-02 22:09:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// Toggle class Active Untuk Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik di luar side bar untuk menghilangkan nav
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
